a108d8e46a4302c5a49fee8296103c2faf3eb9d2
KercyDing <dkx215417@gmail.com>
2026-08-03 12:13:39 +0800

    chore: 调整构建分块警告阈值

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig(async () => ({
     outDir: "../dist",
     emptyOutDir: true,
     assetsInlineLimit: 0,
+    chunkSizeWarningLimit: 600,
   },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
